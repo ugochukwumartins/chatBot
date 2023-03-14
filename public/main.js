45407b2e284179fb891406d866d51bc33aec294a
user <uokorocha@icitsolutions.com>
2023-03-14 19:22:31 +0100

    added some query function

--- public/main.js
+++ public/main.js
@@ -3,6 +3,8 @@ const btn=document.getElementById("btn");
 const message=document.getElementById("messages");
 
 
+let data;
+
 const socket = io();
  
 
@@ -14,7 +16,11 @@ socket.on("connect", ()=>{
     }
  })
     .then(response => response.json())
-    .then(text => console.log( 'helo :',text.orders))
+    .then(text =>{
+
+data= text.orders
+     // console.log( 'helo :', data)
+    })
 
 
   var text =["Select 1 to Place an order", "Select 99 to checkout order" ,"Select 98 to see order history"," Select 97 to see current order", "Select 0 to cancel order "];
@@ -37,335 +43,334 @@ socket.on("connect", ()=>{
 })
 
 
+//
+function removeDuplicates(arr) {
+  return arr.filter((item,
+      index) => arr.indexOf(item) === index);
+}
+
+
+
+//
+let ordrList= [];
+
+let STAPLEFOOD= [];
+let SOUPS= [];
+let SIDEDISHES= [];
+let MEAT= [];
+
+var addditionalInput;
+
+//
+function addtion(){
+  addditionalInput =  window.prompt("select your order")
+var intval= parseInt(addditionalInput)
+if(intval == 1){
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(intval));
+  message.appendChild(li);
+  if (intval) {
+    
+    socket.emit('new message', intval);
+  }
+  for (var i = 0; i < data.length; i++) {
+    // Create DOM element
+   if(data[i]["categoris"] == "STAPLEFOOD" ){
+    STAPLEFOOD.push(data[i]["type"])
+   }
+  //  var li = document.createElement("li");
+     
+
+}
+for (var i = 0; i < STAPLEFOOD.length; i++) {
+  // Create DOM element
+ var li = document.createElement("li");
+      
+  // Set text of element
+  li.textContent = i+1 +"  "+ STAPLEFOOD[i];
+
+  // Append this element to its parent
+  message.appendChild(li);
+  
+}
+console.log(STAPLEFOOD);
+}
+//
+if(intval == 2){
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(intval));
+  message.appendChild(li);
+  if (intval) {
+    
+    socket.emit('new message', intval);
+  }
+  for (var i = 0; i < data.length; i++) {
+    // Create DOM element
+   if(data[i]["categoris"] == "SOUPS" ){
+    SOUPS.push(data[i]["type"])
+   }
+  //  var li = document.createElement("li");
+     
+
+}
+for (var i = 0; i < SOUPS.length; i++) {
+  // Create DOM element
+ var li = document.createElement("li");
+      
+  // Set text of element
+  li.textContent = i+1 +"  "+ SOUPS[i];
+
+  // Append this element to its parent
+  message.appendChild(li);
+  
+}
+}
+//
+if(intval == 3){
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(intval));
+  message.appendChild(li);
+  if (intval) {
+    
+    socket.emit('new message', intval);
+  }
+  for (var i = 0; i < data.length; i++) {
+    // Create DOM element
+   if(data[i]["categoris"] == "SIDEDISHES" ){
+    SIDEDISHES.push(data[i]["type"])
+   }
+  //  var li = document.createElement("li");
+     
+
+}
+for (var i = 0; i < SIDEDISHES.length; i++) {
+  // Create DOM element
+ var li = document.createElement("li");
+      
+  // Set text of element
+  li.textContent = i+1 +"  "+ SIDEDISHES[i];
+
+  // Append this element to its parent
+  message.appendChild(li);
+  
+}
+}
+//
+if(intval == 4){
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(intval));
+  message.appendChild(li);
+  if (intval) {
+    
+    socket.emit('new message', intval);
+  }
+  for (var i = 0; i < data.length; i++) {
+    // Create DOM element
+   if(data[i]["categoris"] == "MEAT" ){
+    MEAT.push(data[i]["type"])
+   }
+  //  var li = document.createElement("li");
+     
+
+}
+for (var i = 0; i < MEAT.length; i++) {
+  // Create DOM element
+ var li = document.createElement("li");
+      
+  // Set text of element
+  li.textContent = i+1 +"  "+ MEAT[i];
+
+  // Append this element to its parent
+  message.appendChild(li);
+  
+}
+}
+}
+
+
+
+
+
 
 btn.addEventListener("click", function(){
-   
-  
-
-  // let message = inputMessage.value;
-  // console.log(message)
-  // // Prevent markup from being injected into the message
-  // message = cleanInput(message);
-  // // if there is a non-empty message and a socket connection
-  // if (message) {
-  //   inputMessage.value;
-  //  // addChatMessage({ username, message });
-  //   // tell server to execute 'new message' and send along one parameter
-  //   socket.emit('new message', message);
-  // }
 
   var inputMessage = document.getElementById("msg").value;
-   
-  // let message = inputMessage.value;
-  console.log(inputMessage)
+
+  if(inputMessage == 1){
+
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(inputMessage));
   message.appendChild(li);
-  // Prevent markup from being injected into the message
-  // message = cleanInput(message);
-  // if there is a non-empty message and a socket connection
   if (inputMessage) {
     
-   // addChatMessage({ username, message });
-    // tell server to execute 'new message' and send along one parameter
     socket.emit('new message', inputMessage);
   }
-});
 
-
-// $(function() {
-//   const FADE_TIME = 150; // ms
-//   const TYPING_TIMER_LENGTH = 400; // ms
-//   const COLORS = [
-//     '#e21400', '#91580f', '#f8a700', '#f78b00',
-//     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-//     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-//   ];
-
-//   // Initialize variables
-//   const $window = $(window);
-//   const $usernameInput = $('.usernameInput'); // Input for username
-//   const $messages = $('.messages');           // Messages area
-//   const inputMessage = document.getElementsByClassName('inputMessage'); // Input message input box
-
-//   const $loginPage = $('.login.page');        // The login page
-//   const $chatPage = $('.chat.page');          // The chatroom page
-
-//   const socket = io();
-
-//   // Prompt for setting a username
-//   let username ='ugo';
-//   let connected = true;
-//   let typing = false;
-//   let lastTypingTime;
-//   let $currentInput = $usernameInput.focus();
-
-//   const addParticipantsMessage = (data) => {
-//     let message = '';
-//     if (data.numUsers === 1) {
-//       message += `there's 1 participant`;
-//     } else {
-//       message += `there are ${data.numUsers} participants`;
-//     }
-//     log(message);
-//   }
-
-//   // Sets the client's username
-//   const setUsername = () => {
-   
-//     // username = cleanInput($usernameInput.val().trim());
-
-//     // // If the username is valid
-//     // if (username) {
-//     //   $loginPage.fadeOut();
-//     //   $chatPage.show();
-//     //   $loginPage.off('click');
-//      $currentInput = $inputMessage.focus();
-//      socket.emit('add user', 'ugo')
-//     //   // Tell the server your username
-//     //   socket.emit('add user', username);
-//     // }
-//   }
-
-//   // Sends a chat message
-//   function sendMessage(){
-//     // let message = inputMessage.value;
-//     // console.log(message)
-//     // // Prevent markup from being injected into the message
-//     // message = cleanInput(message);
-//     // // if there is a non-empty message and a socket connection
-//     // if (message) {
-//     //   inputMessage.value;
-//     //  // addChatMessage({ username, message });
-//     //   // tell server to execute 'new message' and send along one parameter
-//     //   socket.emit('new message', message);
-//     // }
-
-//     var inputMessage = document.getElementById("msg").value;
-     
-//     // let message = inputMessage.value;
-//     console.log(inputMessage)
-//     // Prevent markup from being injected into the message
-//     // message = cleanInput(message);
-//     // if there is a non-empty message and a socket connection
-//     if (inputMessage) {
+  for (var i = 0; i < data.length; i++) {
+    // Create DOM element
+   console.log(data[i]["categoris"])
+  //  var li = document.createElement("li");
       
-//      // addChatMessage({ username, message });
-//       // tell server to execute 'new message' and send along one parameter
-//       socket.emit('new message', inputMessage);
-//     }
-//   }
+ordrList.push(data[i]["categoris"])
+}
 
-//   // Log a message
-//   const log = (message, options) => {
-//     const $el = $('<li>').addClass('log').text(message);
-//     addMessageElement($el, options);
-//   }
+console.log(ordrList);
+var filterData = removeDuplicates(ordrList)
 
-//   // Adds the visual chat message to the message list
-//   const addChatMessage = (data, options = {}) => {
-//     // Don't fade the message in if there is an 'X was typing'
-//     const $typingMessages = getTypingMessages(data);
-//     if ($typingMessages.length !== 0) {
-//       options.fade = false;
-//       $typingMessages.remove();
-//     }
+for (var i = 0; i < filterData.length; i++) {
+  // Create DOM element
+ var li = document.createElement("li");
+      
+  // Set text of element
+  li.textContent = i+1 +"  "+ filterData[i];
 
-//     const $usernameDiv = $('<span class="username"/>')
-//       .text(data.username)
-//       .css('color', getUsernameColor(data.username));
-//     const $messageBodyDiv = $('<span class="messageBody">')
-//       .text(data.message);
+  // Append this element to its parent
+  message.appendChild(li);
+  
+}
+setTimeout(addtion, 2000);
+  clearTimeout(addtion)
 
-//     const typingClass = data.typing ? 'typing' : '';
-//     const $messageDiv = $('<li class="message"/>')
-//       .data('username', data.username)
-//       .addClass(typingClass)
-//       .append($usernameDiv, $messageBodyDiv);
+console.log(filterData)
+  }
+   
+  // let message = inputMessage.value;
+  if(inputMessage == 99){
 
-//     addMessageElement($messageDiv, options);
-//   }
+  //   var li = document.createElement("li");
+  //   li.appendChild(document.createTextNode(inputMessage));
+  //   message.appendChild(li);
+  //   if (inputMessage) {
+      
+  //     socket.emit('new message', inputMessage);
+  //   }
+  
+  //   for (var i = 0; i < data.length; i++) {
+  //     // Create DOM element
+  //    console.log(data[i]["categoris"])
+  //   //  var li = document.createElement("li");
+        
+  // ordrList.push(data[i]["categoris"])
+  // }
+  
+  // console.log(ordrList);
+  // var filterData = removeDuplicates(ordrList)
+  
+  // for (var i = 0; i < filterData.length; i++) {
+  //   // Create DOM element
+  //  var li = document.createElement("li");
+        
+  //   // Set text of element
+  //   li.textContent = i +"  "+ filterData[i];
+  
+  //   // Append this element to its parent
+  //   message.appendChild(li);
+  // }
+  // console.log(filterData)
+  var li = document.createElement("li");
+  li.textContent = 'No order to place'
+  message.appendChild(li);
+    }
 
-//   // Adds the visual chat typing message
-//   const addChatTyping = (data) => {
-//     data.typing = true;
-//     data.message = 'is typing';
-//     addChatMessage(data);
-//   }
+    if(inputMessage == 98){
 
-//   // Removes the visual chat typing message
-//   const removeChatTyping = (data) => {
-//     getTypingMessages(data).fadeOut(function () {
-//       $(this).remove();
-//     });
-//   }
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(inputMessage));
+      message.appendChild(li);
+      if (inputMessage) {
+        
+        socket.emit('new message', inputMessage);
+      }
+    
+      for (var i = 0; i < data.length; i++) {
+        // Create DOM element
+       console.log(data[i]["categoris"])
+      //  var li = document.createElement("li");
+          
+    ordrList.push(data[i]["categoris"])
+    }
+    
+    console.log(ordrList);
+    var filterData = removeDuplicates(ordrList)
+    
+    for (var i = 0; i < filterData.length; i++) {
+      // Create DOM element
+     var li = document.createElement("li");
+          
+      // Set text of element
+      li.textContent = i +"  "+ filterData[i];
+    
+      // Append this element to its parent
+      message.appendChild(li);
+    }
+    console.log(filterData)
+      }
+      if(inputMessage == 97){
 
-//   // Adds a message element to the messages and scrolls to the bottom
-//   // el - The element to add as a message
-//   // options.fade - If the element should fade-in (default = true)
-//   // options.prepend - If the element should prepend
-//   //   all other messages (default = false)
-//   const addMessageElement = (el, options) => {
-//     const $el = $(el);
-//     // Setup default options
-//     if (!options) {
-//       options = {};
-//     }
-//     if (typeof options.fade === 'undefined') {
-//       options.fade = true;
-//     }
-//     if (typeof options.prepend === 'undefined') {
-//       options.prepend = false;
-//     }
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(inputMessage));
+        message.appendChild(li);
+        if (inputMessage) {
+          
+          socket.emit('new message', inputMessage);
+        }
+      
+        for (var i = 0; i < data.length; i++) {
+          // Create DOM element
+         console.log(data[i]["categoris"])
+        //  var li = document.createElement("li");
+            
+      ordrList.push(data[i]["categoris"])
+      }
+      
+      console.log(ordrList);
+      var filterData = removeDuplicates(ordrList)
+      
+      for (var i = 0; i < filterData.length; i++) {
+        // Create DOM element
+       var li = document.createElement("li");
+            
+        // Set text of element
+        li.textContent = i +"  "+ filterData[i];
+      
+        // Append this element to its parent
+        message.appendChild(li);
+      }
+      console.log(filterData)
+        }
+        if(inputMessage == 0){
 
-//     // Apply options
-//     if (options.fade) {
-//       $el.hide().fadeIn(FADE_TIME);
-//     }
-//     if (options.prepend) {
-//       $messages.prepend($el);
-//     } else {
-//       $messages.append($el);
-//     }
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode(inputMessage));
+          message.appendChild(li);
+          if (inputMessage) {
+            
+            socket.emit('new message', inputMessage);
+          }
+        
+          for (var i = 0; i < data.length; i++) {
+            // Create DOM element
+           console.log(data[i]["categoris"])
+          //  var li = document.createElement("li");
+              
+        ordrList.push(data[i]["categoris"])
+        }
+        
+        console.log(ordrList);
+        var filterData = removeDuplicates(ordrList)
+        
+        for (var i = 0; i < filterData.length; i++) {
+          // Create DOM element
+         var li = document.createElement("li");
+              
+          // Set text of element
+          li.textContent = i +"  "+ filterData[i];
+        
+          // Append this element to its parent
+          message.appendChild(li);
+        }
+        console.log(filterData)
+          }
 
-//     $messages[0].scrollTop = $messages[0].scrollHeight;
-//   }
-
-//   // Prevents input from having injected markup
-//   const cleanInput = (input) => {
-//     return $('<div/>').text(input).html();
-//   }
-
-//   // Updates the typing event
-//   const updateTyping = () => {
-//     if (connected) {
-//       if (!typing) {
-//         typing = true;
-//         socket.emit('typing');
-//       }
-//       lastTypingTime = (new Date()).getTime();
-
-//       setTimeout(() => {
-//         const typingTimer = (new Date()).getTime();
-//         const timeDiff = typingTimer - lastTypingTime;
-//         if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-//           socket.emit('stop typing');
-//           typing = false;
-//         }
-//       }, TYPING_TIMER_LENGTH);
-//     }
-//   }
-
-//   // Gets the 'X is typing' messages of a user
-//   const getTypingMessages = (data) => {
-//     return $('.typing.message').filter(function (i) {
-//       return $(this).data('username') === data.username;
-//     });
-//   }
-
-//   // Gets the color of a username through our hash function
-//   const getUsernameColor = (username) => {
-//     // Compute hash code
-//     let hash = 7;
-//     for (let i = 0; i < username.length; i++) {
-//       hash = username.charCodeAt(i) + (hash << 5) - hash;
-//     }
-//     // Calculate color
-//     const index = Math.abs(hash % COLORS.length);
-//     return COLORS[index];
-//   }
-
-//   // Keyboard events
-
-//   $window.keydown(event => {
-//     console.log('trying');
-//     // Auto-focus the current input when a key is typed
-//     if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-//       $currentInput.focus();
-//     }
-//     // When the client hits ENTER on their keyboard
-//     if (event.which === 13) {
-//       if (username) {
-//         sendMessage();
-//         socket.emit('stop typing');
-//         typing = false;
-//       } else {
-//         setUsername();
-//       }
-//     }
-//   });
-
-//   $inputMessage.on('input', () => {
-//     updateTyping();
-//   });
-
-//   // Click events
-
-//   // Focus input when clicking anywhere on login page
-//   $loginPage.click(() => {
-//     $currentInput.focus();
-//   });
-
-//   // Focus input when clicking on the message input's border
-//   $inputMessage.click(() => {
-//     $inputMessage.focus();
-//   });
-
-//   // Socket events
-
-//   // Whenever the server emits 'login', log the login message
-//   socket.on('login', (data) => {
-//     connected = true;
-//     // Display the welcome message
-//     const message = 'Welcome to Socket.IO Chat – ';
-//     log(message, {
-//       prepend: true
-//     });
-//     addParticipantsMessage(data);
-//   });
-
-//   // Whenever the server emits 'new message', update the chat body
-//   socket.on('new message', (data) => {
-//     addChatMessage(data);
-//   });
-
-//   // Whenever the server emits 'user joined', log it in the chat body
-//   socket.on('user joined', (data) => {
-//     log(`${data.username} joined`);
-//     addParticipantsMessage(data);
-//   });
-
-//   // Whenever the server emits 'user left', log it in the chat body
-//   socket.on('user left', (data) => {
-//     log(`${data.username} left`);
-//     addParticipantsMessage(data);
-//     removeChatTyping(data);
-//   });
-
-//   // Whenever the server emits 'typing', show the typing message
-//   socket.on('typing', (data) => {
-//     addChatTyping(data);
-//   });
-
-//   // Whenever the server emits 'stop typing', kill the typing message
-//   socket.on('stop typing', (data) => {
-//     removeChatTyping(data);
-//   });
-
-//   socket.on('disconnect', () => {
-//     log('you have been disconnected');
-//   });
-
-//   socket.io.on('reconnect', () => {
-//     log('you have been reconnected');
-//     if (username) {
-//       socket.emit('add user', username);
-//     }
-//   });
-
-//   socket.io.on('reconnect_error', () => {
-//     log('attempt to reconnect has failed');
-//   });
-
-// });
+});
 
